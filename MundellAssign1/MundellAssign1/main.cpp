c972291c206def7abeeb2cb3465249ac2ebcdc59
AdmiralOct <buildgamessnotbombs@gmail.com>
2018-01-20 21:36:39 -0500

    Finished fixing up stuff
    The unsorted third highest algorithm is operational.
    As well as the Sorted Algorithm.

--- MundellAssign1/MundellAssign1/main.cpp
+++ MundellAssign1/MundellAssign1/main.cpp
@@ -15,6 +15,12 @@ void DisplayArray(int arr[], int size) {
 	}
 }
 
+void DisplayTopTen(vector<int> input) {
+	for (int i = input.size() - 10; i < input.size(); i++) {
+		cout << input.at(i) << ',' ;
+	}
+}
+
 int HighestNumber(vector<int> input) {
 	int highestNumber = 0;
 	for (int i = 0; i < input.size(); i++) {
@@ -25,24 +31,67 @@ int HighestNumber(vector<int> input) {
 	return highestNumber;
 }
 
+int HighestVectorNumber(vector<int> input) {
+
+	int highestNum = 0;
+	int size = input.size();
+	highestNum = input.at(size);
+
+	return highestNum;
+}
+
 int ThirdHighestNumber(vector<int> input) {
-	vector<int> HighestThree(3);
+	vector<int> HighestThree(3);	
+	HighestThree.at(0) = 0;
+	HighestThree.at(1) = 0;
+	HighestThree.at(2) = 0;
 	for (int i = 0; i < input.size(); i++) {
-		if (i > HighestThree.at(1)) {
+		if (input.at(i) >= HighestThree.at(0)) {
+			HighestThree.at(2) = HighestThree.at(1);
+			HighestThree.at(1) = HighestThree.at(0);
+			HighestThree.at(0) = input.at(i);
+		
+		}
+		else if (input.at(i) >= HighestThree.at(1)) {
+			HighestThree.at(2) = HighestThree.at(1);
 			HighestThree.at(1) = input.at(i);
 		}
-		else if (i > HighestThree.at(2)) {
-			HighestThree.at(1) = input.at(i);
-		}
-		else if (i > HighestThree.at(3)) {
-			HighestThree.at(3) = input.at(i);
+		else if (input.at(i) >= HighestThree.at(2)) {
+			HighestThree.at(2) = input.at(i);
 		}
 		else {
 			//nothing
+			//cout << "Not Higher than anything." << endl;
 		}
 	}
 
-	return HighestThree.at(3);
+	return HighestThree.at(2);
+}
+
+int SortedThirdHighestNumber(vector<int> input) {
+	vector<int> HighestThree(3);
+	HighestThree.at(0) = 0;
+	HighestThree.at(1) = 0;
+	HighestThree.at(2) = 0;
+
+	for (int i = input.size() - 1; i > 0; i--) {
+		if (input.at(i) >= HighestThree.at(0)) {
+			HighestThree.at(0) = input.at(i);
+		}
+		else if (input.at(i) >= HighestThree.at(1)) {
+			HighestThree.at(1) = input.at(i);
+		}
+		else if (input.at(i) >= HighestThree.at(2)) {
+			HighestThree.at(2) = input.at(i);
+			break;
+		}
+		else {
+			//nothing
+			cout << "Was most likely equal." << endl;
+		}
+	}
+
+	return HighestThree.at(2);
 }
 int main() {
 	srand(time(0));
@@ -53,14 +102,21 @@ int main() {
 		RandomNumbers[i] = rand() % 1000;
 	}
 
-	DisplayArray(RandomNumbers.data(), 1000);
+	//DisplayArray(RandomNumbers.data(), 1000);
 
-	cout << endl << endl << HighestNumber(RandomNumbers);
+	cout << endl << HighestNumber(RandomNumbers) << endl;
+
+	cout << endl << ThirdHighestNumber(RandomNumbers) << endl;
 
 	sort(RandomNumbers.begin(), RandomNumbers.end());
 
-	DisplayArray(RandomNumbers.data(), 1000);
+	//DisplayArray(RandomNumbers.data(), 1000);
 
+	cout << endl << HighestNumber(RandomNumbers) << endl;
 
+	DisplayTopTen(RandomNumbers);
+	cout << endl << endl;
+	//cout << endl << ThirdHighestNumber(RandomNumbers) << endl;
+	cout << endl << SortedThirdHighestNumber(RandomNumbers) << endl;
 	return 0;
 }
